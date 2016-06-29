ec404855e2f709be5025e169ecddcf7e30aa79de
Sai Vishal <saivishal2001@gmail.com>
2016-06-29 20:58:37 +0530

    DT

---
+++ dt.sh
@@ -0,0 +1,7 @@
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="DroidThug"
+export KBUILD_BUILD_HOST="EvoqueUnit"
+git config user.name "Sai Vishal"
+git config user.email au@aurum.cf
+# To be used before building!
