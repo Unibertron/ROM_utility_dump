71c0b3d13dc5efb520849a1347b0ac158f0d6517
Sai Vishal <saivishal2001@gmail.com>
2016-06-29 20:59:58 +0530

    Update and rename dsm to dsm.sh

--- dsm.sh
+++ dsm.sh
@@ -2,3 +2,6 @@ export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER="darshan1205"
 export KBUILD_BUILD_HOST="darshan-desktop"
+git config user.name "Darshan Manjrekar"
+git config user.email darshanmanjrekar@gmail.com
+# To be used before building!
