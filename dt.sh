e1dc8f1a8b88e0654a4bd979deaf3280fc656088
Sai Vishal <saivishal2001@gmail.com>
2016-07-02 18:57:51 +0530

    Go Global

--- dt.sh
+++ dt.sh
@@ -1,7 +1,5 @@
-export ARCH=arm64
-export SUBARCH=arm64
 export KBUILD_BUILD_USER="DroidThug"
 export KBUILD_BUILD_HOST="EvoqueUnit"
-git config user.name "Sai Vishal"
-git config user.email au@aurum.cf
+git config --global user.name "Sai Vishal"
+git config --global user.email "au@aurum.cf"
 # To be used before building!
